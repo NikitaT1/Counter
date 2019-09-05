59861a37138669b56f67e4be71799bc38422bae3
NikitaT1 <nik.tati1@gmail.com>
2019-09-05 17:44:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+    state = {
+        numbers: 0
+    };
+
+    saveState = (props) => {
+        let stateAsString = JSON.stringify (this.state);
+        localStorage.setItem ("our-state", stateAsString);
+    };
+
+    plus = () => {
+        if (this.state.numbers <=9)
+        { let newNumberPlus = this.state.numbers+1;
+        this.setState({numbers: newNumberPlus}, () => {this.saveState()})}
+        else {{this.zeroing()} (alert('too much!'))}
+    };
+
+    minus = () => {
+        if (this.state.numbers >= -9)
+        {let newNumberMinus = this.state.numbers-1;
+        this.setState({numbers: newNumberMinus}, () => {this.saveState()})}
+        else {{this.zeroing()} (alert('too less!'))}
+    };
+
+    zeroing = () => {this.setState({numbers: 0}, () => {this.saveState()})};
+
+    putNumber = () => {}
+
+    addNumber = () => {}
+
+    onKeyPress = (e) => {
+        if (e.key === "Enter") {
+            this.addNumber()
+        }
+    };
+
+    render = () => {
+
+        const element = (
+            <h1>
+                {this.state.numbers}
+            </h1>
+        );
+
+        return (
+            <div className="App">
+                <div className="one">
+                        <h2 className="color">TOTAL</h2>
+                        <div>
+                        </div>
+                    <div className="number">{element}</div>
+                    <div>
+                        <button onClick={this.minus}>Minus</button>
+                        <button onClick={this.plus}>Plus</button>
+                        <button onClick={this.zeroing}>Zeroing</button>
+                        <div>
+                        <input type="text" placeholder="new number"
+                               onChange={()=> {
+                                       if (this.value === Number) {
+                                           {this.setState({numbers: this.value}, () => {this.saveState()})};
+                                       }
+                                   }}
+                               onKeyPress={this.onKeyPress}
+                               value={()=> alert('Bum2!')}
+                        />
+                            <button onClick={()=> alert('Bum1!')}>Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
---
+++ src/numbersChange.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+
+
+class numbersChange extends React.Component {
+
+    saveState = (props) => {
+        let stateAsString = JSON.stringify (this.props.state);
+        localStorage.setItem ("our-state", stateAsString);
+    };
+
+    plus = (props) => {
+        if (this.props.state.numbers <=9)
+        { let newNumberPlus = this.props.state.numbers+1;
+        this.props.setState({numbers: newNumberPlus}, () => {this.props.saveState()})}
+        else {this.zeroing()}
+    };
+
+    minus = (props) => {
+        if (this.props.state.numbers >= -9)
+        {let newNumberMinus = this.props.state.numbers-1;
+        this.props.setState({numbers: newNumberMinus}, () => {this.props.saveState()})}
+        else {this.zeroing()}
+    };
+
+    zeroing = (props) => {this.porps.setState({numbers: 0}, () => {this.props.saveState()})};
+
+
+
+    render = () => {
+
+        return (
+            <div className="App">
+                <div className="one">
+                    <div>
+                        <button onClick={this.minus}>Minus</button>
+                        <button onClick={this.plus}>Plus</button>
+                        <button onClick={this.zeroing}>Zeroing</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default numbersChange;
+
