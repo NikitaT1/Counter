8f34237c13e31eb573dd15348b9389c2705e98ae
NikitaT1 <nik.tati1@gmail.com>
2019-09-09 13:40:59 +0300

    add max and min numbers

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 class App extends React.Component {
 
     state = {
-        numbers: 0
+        numbers: 0,
+        maxNumber: 9,
+        minNumber: -9
     };
 
     saveState = (props) => {
@@ -13,14 +15,14 @@ class App extends React.Component {
     };
 
     plus = () => {
-        if (this.state.numbers <=9)
+        if (this.state.numbers <= this.state.maxNumber)
         { let newNumberPlus = this.state.numbers+1;
         this.setState({numbers: newNumberPlus}, () => {this.saveState()})}
         else {{this.zeroing()} (alert('too much!'))}
     };
 
     minus = () => {
-        if (this.state.numbers >= -9)
+        if (this.state.numbers >= this.state.minNumber)
         {let newNumberMinus = this.state.numbers-1;
         this.setState({numbers: newNumberMinus}, () => {this.saveState()})}
         else {{this.zeroing()} (alert('too less!'))}
