b27b8724176c23664653660d8a61e1a9d7f25b3e
NikitaT1 <nik.tati1@gmail.com>
2019-09-09 22:34:09 +0300

    trying to add new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+/*import Settings from "./Settings";*/
 
 class App extends React.Component {
 
@@ -8,23 +9,18 @@ class App extends React.Component {
         maxNumber: 9,
         minNumber: -9,
         inputMax: 0,
-        inputMin: 0
+        inputMin: 0,
     };
 
-    /*saveState = () => {
-        let stateAsString = JSON.stringify (this.state);
-        localStorage.setItem ("our-state", stateAsString);
-    };*/
-
     plus = () => {
-        if (this.state.numbers <= this.state.maxNumber-1)
+        if (this.state.numbers < this.state.maxNumber)
         { let newNumberPlus = this.state.numbers+1;
         this.setState({numbers: newNumberPlus})}
         /*else {{this.zeroing()} (alert('too much!'))}*/
     };
 
     minus = () => {
-        if (this.state.numbers >= this.state.minNumber+1)
+        if (this.state.numbers > this.state.minNumber)
         {let newNumberMinus = this.state.numbers-1;
         this.setState({numbers: newNumberMinus})}
         /*else {{this.zeroing()} (alert('too less!'))}*/
@@ -32,57 +28,58 @@ class App extends React.Component {
 
     zeroing = () => {this.setState({numbers: 0})};
 
-    maxNumberChanged = (e) => {this.setState({inputMax: e.currentTarget.value})};
+    maxNumberChanged = (e) => {
+        if (e.currentTarget.value > 0)
+        {this.setState({inputMax: e.currentTarget.value})}
+    };
 
-    maxNumberAdd = () => {this.setState({maxNumber: this.state.inputMax})};
+    maxNumberAdd = () => {
+        this.setState({maxNumber: this.state.inputMax})
+    };
 
+    minNumberChanged = (e) => {
+        if (e.currentTarget.value < 0)
+        this.setState({inputMin: e.currentTarget.value})};
+
+    minNumberAdd = () => {
+        this.setState({minNumber: this.state.inputMin})
+    };
 
     render = () => {
 
-        const element = (
-            <h1>
-                {this.state.numbers}
-            </h1>
+        const element1 = (
+            <div className="one">
+                <h2 className="color">TOTAL</h2>
+                <h1>{this.state.numbers}</h1>
+                    <div> MaxNumber: {this.state.maxNumber} <div>
+                            MinNumber: {this.state.minNumber}
+                        </div>
+                    </div>
+                <div>
+                    <button onClick={this.minus}>Minus</button>
+                    <button onClick={this.plus}>Plus</button>
+                    <button onClick={this.zeroing}>Reset</button>
+                </div>
+            </div>
         );
 
         const element2 = (
-            <div>
-                MaxNumber: {this.state.maxNumber}
-            <div>
-                MinNumber: {this.state.minNumber}
+            <div className="two">
+                <input type="text" placeholder="new maxNumber"
+                       onChange={this.maxNumberChanged}/>
+                <button onClick={this.maxNumberAdd}>Add</button>
+                <div>
+                    <input type="text" placeholder="new minNumber"
+                           onChange={this.minNumberChanged}/>
+                    <button onClick={this.minNumberAdd}>Add</button>
+                </div>
             </div>
-            </div>
-        )
+        );
 
         return (
-            <div className="App">
-                <div className="one">
-                        <h2 className="color">TOTAL</h2>
-                        <div>
-                        </div>
-                    <div className="number">{element}</div>
-                    <div>{element2}</div>
-                    <div>
-                        <button onClick={this.minus}>Minus</button>
-                        <button onClick={this.plus}>Plus</button>
-                        <button onClick={this.zeroing}>Zeroing</button>
-                        <div>
-                        <input type="text" placeholder="new maxNumber"
-                              /*onChange={this.maxNumberAdd} value={this.state.maxNumber}*/
-                            onChange={this.maxNumberChanged}
-                        />
-                            <button onClick={this.maxNumberAdd}>Add</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="two">
-                    <div>
-                        <input type="text" placeholder="new number"/>
-                    </div>
-                    <button onClick={()=>{alert("Bum!")}}>Bum!</button>
-                    <button onClick={()=>{alert("Bum!")}}>Bum!</button>
-                </div>
-            </div>
+
+                <div> {element1} {element2} </div>
+
         );
     }
 }
