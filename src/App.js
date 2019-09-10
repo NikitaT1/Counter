f2f7c8165a716e46173845e122fd60cbd87431f7
NikitaT1 <nik.tati1@gmail.com>
2019-09-10 22:41:11 +0300

    prefinal restyling, refactoring

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ class App extends React.Component {
 
     state = {
         numbers: 0,
-        maxNumber: 2,
-        minNumber: -2,
+        maxNumber: 3,
+        minNumber: -3,
         inputMax: 0,
         inputMin: 0,
         addNumbers: 0,
         SetupActive: false,
+        test: "Bum!"
     };
 
     plus = () => {
+        {this.disactivateSettings()}
         if (this.state.numbers < this.state.maxNumber)
         { let newNumberPlus = ++this.state.numbers;
         this.setState({numbers: newNumberPlus})}
@@ -22,53 +24,74 @@ class App extends React.Component {
     };
 
     minus = () => {
+        {this.disactivateSettings()}
         if (this.state.numbers > this.state.minNumber)
         {let newNumberMinus = this.state.numbers-1;
         this.setState({numbers: newNumberMinus})}
         /*else {{this.zeroing()} (alert('too less!'))}*/
     };
 
-    zeroing = () => {this.setState({numbers: 0})};
+    zeroing = () => {
+        {this.disactivateSettings()}
+        this.setState({numbers: 0})};
+
+    disactivateTotal = () => {this.setState({SetupActive: true})};
+
+    disactivateSettings = () => {this.setState({SetupActive: false})};
 
     maxNumberChanged = (e) => {
+        {this.disactivateTotal()}
         if (e.currentTarget.value > 0)
         {this.setState({inputMax: e.currentTarget.value})}
         else {this.setState({inputMin: this.state.maxNumber})}
     };
 
     maxNumberAdd = (e) => {
+        {this.disactivateTotal()}
         this.setState({maxNumber: (Math.round(this.state.inputMax))});
     };
 
     minNumberChanged = (e) => {
+        {this.disactivateTotal()}
         if (e.currentTarget.value < 0)
         {this.setState({inputMin: e.currentTarget.value})}
     else {this.setState({inputMin: e.currentTarget.value*-1})}
     };
 
     minNumberAdd = (e) => {
+        {this.disactivateTotal()}
         this.setState({minNumber: (Math.round(this.state.inputMin))})
     };
 
     NumberChanged = (e) => {
+        {this.disactivateTotal()}
        this.setState({addNumbers: e.currentTarget.value})
     };
 
     NumberAdd = (e) => {
+        {this.disactivateTotal()}
         if (this.state.addNumbers < this.state.minNumber || this.state.addNumbers > this.state.maxNumber)
-        {alert('Error!')}
+        {{this.zeroing()} (alert('FUCKING WRONG NUMBER, DUDE!'))}
         else {
         this.setState({numbers: this.state.addNumbers})}
     };
 
+
+    enterValue = "Enter values";
+
     render = () => {
 
-        /*const newOne = this.state.SetupActive ? "todoList-task done" : "todoList-task";*/
+        /*const enterValue = "Enter values and press button";*/
+        const disactTotal = this.state.SetupActive ? "one noactive" : "one";
+        const disactSettings = this.state.SetupActive ? "two" : "two noactive";
+        const showTotal = this.state.SetupActive ? this.enterValue : this.state.numbers;
 
         const element1 = (
-            <div className="one">
+            <div className={disactTotal}>
+                <span onClick={this.disactivateSettings}>
                 <h2 className="color">TOTAL</h2>
-                <h1>{this.state.numbers}</h1>
+                </span>
+                <h1>{showTotal}</h1>
                     <div> MaxNumber: {this.state.maxNumber} <div>
                             MinNumber: {this.state.minNumber}
                         </div>
@@ -82,18 +105,20 @@ class App extends React.Component {
         );
 
         const element2 = (
-            <div className="two">
+            <div className={disactSettings}>
+                <span onClick={this.disactivateTotal}>
                 <h2 className="color">SETTINGS</h2>
+                </span>
                 <input type="text" placeholder="new Number"
-                       onChange={this.NumberChanged}/>
-                <button onClick={this.NumberAdd}>AddMax</button>
+                       onChange={this.NumberChanged} onClick={this.disactivateTotal}/>
+                <button onClick={this.NumberAdd}>AddNum</button>
                 <div>
                     <input type="text" placeholder="new maxNumber"
-                           onChange={this.maxNumberChanged}/>
+                           onChange={this.maxNumberChanged} onClick={this.disactivateTotal}/>
                     <button onClick={this.maxNumberAdd}>AddMax</button>
                     <div>
                     <input type="text" placeholder="new minNumber"
-                           onChange={this.minNumberChanged}/>
+                           onChange={this.minNumberChanged} onClick={this.disactivateTotal}/>
                     <button onClick={this.minNumberAdd}>AddMin</button>
                 </div>
             </div>
@@ -102,7 +127,7 @@ class App extends React.Component {
 
         return (
 
-                <div class="commonStyle"> {element1}
+                <div className="commonStyle"> {element1}
             <div> {element2} </div> </div>
 
         );
